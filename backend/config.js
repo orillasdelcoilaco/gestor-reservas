87b023d8f910880a1c14757ec20292206e8ea0a6
orillasdelcoilaco <orillasdelcoilaco@gmail.com>
2025-08-28 21:23:36 -0400

    fix(drive): Corregir el ID de la carpeta padre para la subida de archivos

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,7 @@ const config = {
   DRIVE_FOLDER_ID: '1ED6xW0Ti_nBgRr_EzbYJ9XG1AxvfiUrx', // <-- Asegúrate de que este ID sea el correcto
 
     // ID de la carpeta compartida en "Mi Unidad" para guardar los documentos de reservas.
-  DOCUMENTS_PARENT_FOLDER_ID: '1xAZcGboZeYEf9hvYnmAy8jjUjK8X6PwE',
+  DOCUMENTS_PARENT_FOLDER_ID: '1Nl6Z5tAmm4nwTfwDfWgEpjT298VmusLX',
 
   // Patrones para identificar los archivos de reporte.
   SODC_FILE_PATTERN: 'mphb-bookings',
